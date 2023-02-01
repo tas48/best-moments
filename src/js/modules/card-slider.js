44dbc89aaf5f8da5eda97baba950cfc06df99bec
thalisson <talissonlps11@gmail.com>
2023-02-01 12:05:19 -0300

    🐛 Mobile scroll's bug fixed

--- src/js/modules/card-slider.js
+++ src/js/modules/card-slider.js
@@ -12,12 +12,12 @@ function cardSlider(){
 
     for (var i of cards){
       if(current == 0){i.style.left = "0px";}
-      if(current == 1){i.style.left = "-220px";}
-      if(current == 2){i.style.left = "-440px";}
-      if(current == 3){i.style.left = "-660px";}
-      if(current == 4){i.style.left = "-880px";}
-      if(current == 5){i.style.left = "-1100px";}
-      if(current == 6){i.style.left = "-1320px";}
+      if(current == 1){i.style.left = "-199px";}
+      if(current == 2){i.style.left = "-398px";}
+      if(current == 3){i.style.left = "-597px";}
+      if(current == 4){i.style.left = "-796px";}
+      if(current == 5){i.style.left = "-995px";}
+      if(current == 6){i.style.left = "-1194px";}
   
     }
   });
@@ -27,14 +27,14 @@ function cardSlider(){
     if(current < 0){ current = 0;}
     console.log(current);
 
-    for (var i of cards){
-      if(current == 0){i.style.left = "0px"}
-      if(current == 1){i.style.left = "-220px";}
-      if(current == 2){i.style.left = "-440px";}
-      if(current == 3){i.style.left = "-660px";}
-      if(current == 4){i.style.left = "-880px";}
-      if(current == 5){i.style.left = "-1100px";}
-      if(current == 6){i.style.left = "-1320px";}
+    for (let i of cards){
+      if(current == 0){i.style.left = "0px";}
+      if(current == 1){i.style.left = "-199px";}
+      if(current == 2){i.style.left = "-398px";}
+      if(current == 3){i.style.left = "-597px";}
+      if(current == 4){i.style.left = "-796px";}
+      if(current == 5){i.style.left = "-995px";}
+      if(current == 6){i.style.left = "-1194px";}
   
     }
   });

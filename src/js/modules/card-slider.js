6742725cd1f1349e4b357da511cec641711b4e52
thalisson <talissonlps11@gmail.com>
2023-02-02 12:50:31 -0300

    🔨 update slider scroll

--- src/js/modules/card-slider.js
+++ src/js/modules/card-slider.js
@@ -7,18 +7,14 @@ function cardSlider(){
 
   arrowNext.addEventListener('click', () =>{
     current++;
-    if(current >= 6){ current = 6;}
     console.log(current);
-
-    for (var i of cards){
-      if(current == 0){i.style.left = "0px";}
-      if(current == 1){i.style.left = "-199px";}
-      if(current == 2){i.style.left = "-398px";}
-      if(current == 3){i.style.left = "-597px";}
-      if(current == 4){i.style.left = "-796px";}
-      if(current == 5){i.style.left = "-995px";}
-      if(current == 6){i.style.left = "-1194px";}
-  
+    if(current >= 4){ current = 4;}
+    for (let i of cards){
+      if(current == 0){i.style.left = "0%";}
+      if(current == 1){i.style.left = "-25%";}
+      if(current == 2){i.style.left = "-50%";}
+      if(current == 3){i.style.left = "-75%";}
+      if(current == 4){i.style.left = "-100%";}
     }
   });
 
@@ -28,13 +24,12 @@ function cardSlider(){
     console.log(current);
 
     for (let i of cards){
-      if(current == 0){i.style.left = "0px";}
-      if(current == 1){i.style.left = "-199px";}
-      if(current == 2){i.style.left = "-398px";}
-      if(current == 3){i.style.left = "-597px";}
-      if(current == 4){i.style.left = "-796px";}
-      if(current == 5){i.style.left = "-995px";}
-      if(current == 6){i.style.left = "-1194px";}
+      if(current == 0){i.style.left = "0%";}
+      if(current == 1){i.style.left = "-25%";}
+      if(current == 2){i.style.left = "-50%";}
+      if(current == 3){i.style.left = "-75%";}
+      if(current == 4){i.style.left = "-100%";}
+    
   
     }
   });

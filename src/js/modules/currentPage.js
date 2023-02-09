80f3019b164fff15877c495217c1cd78c83eb6d0
thalisson <talissonlps11@gmail.com>
2023-02-09 20:39:21 -0300

    🚧📱 working on footer responsive design

--- src/js/modules/currentPage.js
+++
@@ -1,7 +0,0 @@
-function indicatePage(){
-  const navLink = document.querySelectorAll(".link");
-  let currentURL = window.location.href;
-  console.log(currentURL); 
-}
-
-export default indicatePage;
--- src/js/pages/not-found.js
+++ src/js/pages/not-found.js
@@ -1,5 +1,3 @@
 import renderMenu from '../modules/mobile-navbar.js';
-import currentPage from '../modules/current-page.js';
 
 renderMenu();
-currentPage();

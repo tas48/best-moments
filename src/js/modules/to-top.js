4f0f4d4016bb7830f6e2ea39e49b5167e74cce0c
thalisson <talissonlps11@gmail.com>
2023-02-13 14:38:42 -0300

    🐛 space under the footer's bug fixed

--- src/js/modules/to-top.js
+++ src/js/modules/to-top.js
@@ -1,6 +1,5 @@
 function toTop(){
   const topButton = document.querySelector('.to-top');
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
   
   topButton.addEventListener('click', () => {
     document.body.scrollTop = 0;

1f013a9beef9dfaf525152b47a6f68365496df44
thalisson <talissonlps11@gmail.com>
2023-02-26 14:12:43 -0300

    💩 fixing problems and refactoring some bad code

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,3 +1,11 @@
 import renderMenu from '../modules/mobile-navbar.js';
+import stickyNavbar from '../modules/sticky-navbar.js';
+import imageSlider from '../modules/image-slider.js';
+import toTop from '../modules/to-top.js';
+import currentYear from '../modules/current-year.js';
 
 renderMenu();
+// stickyNavbar();
+// imageSlider();
+toTop();
+currentYear();
